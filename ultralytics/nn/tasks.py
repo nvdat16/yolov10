d5faf6ba544c0baf19da7d520c271fd5808f50f0
nvdat16 <nguyenvandat160105@gmail.com>
2025-09-02 01:27:02 +0700

    cswin-transformer registration

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -53,7 +53,9 @@ from ultralytics.nn.modules import (
     PSA,
     SCDown,
     RepVGGDW,
-    v10Detect
+    v10Detect,
+    CSWinTransformer,
+    Stage
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -888,7 +890,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             RepC3,
             PSA,
             SCDown,
-            C2fCIB
+            C2fCIB,
+            CSWinTransformer,
+            Stage
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
