ab65994c8a7f2cf13fe378fd7f71e0d1168de3b1
nvdat16 <nguyenvandat160105@gmail.com>
2025-09-02 10:02:51 +0700

    global registration cswin-transformer

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -53,14 +53,10 @@ from ultralytics.nn.modules import (
     PSA,
     SCDown,
     RepVGGDW,
-    v10Detect
+    v10Detect,
+    CSWinTransformer,
+    Stage
 )
-
-from ultralytics.nn.modules import CSWinTransformer
-globals()['CSWinTransformer'] = CSWinTransformer
-
-from ultralytics.nn.modules import Select
-globals()['Select'] = Select
 
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
