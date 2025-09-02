291d3a2e0d6492f5a1887a172d700a6cbf1473fd
nvdat16 <nguyenvandat160105@gmail.com>
2025-09-02 23:32:29 +0700

    add cswin transformer backbone

--- ultralytics/nn/modules/cswin.py
+++ ultralytics/nn/modules/cswin.py
@@ -246,7 +246,7 @@ class CSWinTransformer(nn.Module):
     """ Vision Transformer with support for patch or hybrid CNN input stage
     """
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=96, depth=[2,2,6,2], split_size = [3,5,7],
-                 num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 num_heads=[2,4,8,16], mlp_ratio=4., qkv_bias=True, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
                  drop_path_rate=0., hybrid_backbone=None, norm_layer=nn.LayerNorm, use_chk=False):
         super().__init__()
         self.num_classes = num_classes
