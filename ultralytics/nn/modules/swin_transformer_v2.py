a3d1fabd1dfe590a686ec8a36bca25bfaac9af25
nvdat16 <nguyenvandat160105@gmail.com>
2025-10-29 19:14:22 +0700

    add swin_transformer_v2

--- ultralytics/nn/modules/swin_transformer_v2.py
+++ ultralytics/nn/modules/swin_transformer_v2.py
@@ -361,7 +361,7 @@ class PatchMerging(nn.Module):
         super().__init__()
         self.input_resolution = input_resolution
         self.dim = dim
-        self.reduction = nn.Linear(4 * dim, 2 * dim, bias=False)
+        self.reduction = nn.Linear(2 * dim, 4 * dim, bias=False)
         self.norm = norm_layer(2 * dim)
 
     def forward(self, x):
