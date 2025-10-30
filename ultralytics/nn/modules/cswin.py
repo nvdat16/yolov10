14cd3c8c60c2fc60c4a30f0048e02ad7c0cbd85f
nvdat16 <nguyenvandat160105@gmail.com>
2025-10-30 21:56:22 +0700

    add cswin_transformer

--- ultralytics/nn/modules/cswin.py
+++ ultralytics/nn/modules/cswin.py
@@ -20,24 +20,6 @@ import torch.utils.checkpoint as checkpoint
 import numpy as np
 import time
 
-def _cfg(url='', **kwargs):
-    return {
-        'url': url,
-        'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
-        'crop_pct': .9, 'interpolation': 'bicubic',
-        'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD,
-        'first_conv': 'patch_embed.proj', 'classifier': 'head',
-        **kwargs
-    }
-
-
-default_cfgs = {
-    'cswin_224': _cfg(),
-    'cswin_384': _cfg(
-        crop_pct=1.0
-    ),
-
-}
 
 
 class Mlp(nn.Module):
