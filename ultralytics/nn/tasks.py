1fa0bb88edd10ef1ceb7883ff174cec77c2272a6
nvdat16 <nguyenvandat160105@gmail.com>
2025-11-05 11:22:55 +0700

    add cswin_transformer

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1006,23 +1006,23 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [c1, c2, *args[1:]]
         elif m is CBFuse:
             c2 = ch[f[-1]]
-        elif m is CSWinTransformer:
-            cswin = CSWinTransformer(*args)
-            c2 = list(cswin.out_channels)  # lấy đúng out_channels từ CSWinTransformer
-            for c in c2:
-                ch.append(c)
+        # elif m is CSWinTransformer:
+        #     cswin = CSWinTransformer(*args)
+        #     c2 = list(cswin.out_channels)  # lấy đúng out_channels từ CSWinTransformer
+        #     for c in c2:
+        #         ch.append(c)
         elif m is SwinTransformerV2:
             swin = SwinTransformerV2(*args)
             c2 = list(swin.out_channels)  # lấy đúng out_channels từ SwinTransformerV2
             for c in c2:
                 ch.append(c)
-        elif m is Stage:
-            idx = args[0]
-            # ch[f] là list các channels output của backbone, lấy đúng index
-            if isinstance(ch[f], (list, tuple)):
-                c2 = ch[f][idx]
-            else:
-                c2 = ch[f]
+        # elif m is Stage:
+        #     idx = args[0]
+        #     # ch[f] là list các channels output của backbone, lấy đúng index
+        #     if isinstance(ch[f], (list, tuple)):
+        #         c2 = ch[f][idx]
+        #     else:
+        #         c2 = ch[f]
         else:
             c2 = ch[f]
 
